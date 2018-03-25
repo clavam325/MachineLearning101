7bee5750d1be98d82a9cfc67f3ddd473a1913682
clavam325 <jyadav2007@gmail.com>
2018-03-26 00:11:51 +0530

    Week3 Logitstic Regression

---
+++ Week3/ex2/sigmoid.m
@@ -0,0 +1,23 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+row = rows(z);
+col = columns(z);
+
+for r = 1:row
+	for c = 1:col
+		g(r,c) = 1/(1 + exp(-z(r,c)));
+	endfor
+endfor
+
+% =============================================================
+
+end
